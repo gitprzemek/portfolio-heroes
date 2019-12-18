9340afd1adfe71f3f695d363b2f85e889c5a2b5c
gitprzemek <przemyslaw.sz.home@gmail.com>
2019-12-18 21:58:23 +0100

    add borders to main wrapper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationPanelComponent } from './components/navigation-panel/navigation-panel.component';
+import { MainPanelComponent } from './components/main-panel/main-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationPanelComponent,
+    MainPanelComponent
   ],
   imports: [
     BrowserModule
