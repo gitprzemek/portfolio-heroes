f438f397a148c7cb8cacbaa9a4138a46a4f252e1
gitprzemek <przemyslaw.sz.home@gmail.com>
2019-12-28 19:45:31 +0100

    add navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavigationPanelComponent } from './components/navigation-panel/navigation-panel.component';
 import { MainPanelComponent } from './components/main-panel/main-panel.component';
+import { ActionFieldComponent } from './components/action-field/action-field.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationPanelComponent,
-    MainPanelComponent
+    MainPanelComponent,
+    ActionFieldComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/action-field/action-field.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-action-field',
+  templateUrl: './action-field.component.html',
+  styleUrls: ['./action-field.component.scss']
+})
+export class ActionFieldComponent implements OnInit {
+  @Input() type: string;
+  @Input() content: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
