41aa8e8f021e2394ef5b94ba7254d1f0406f3e7d
gitprzemek <przemyslaw.sz.home@gmail.com>
2020-01-01 19:38:51 +0100

    add right panel and icons

--- src/app/components/action-field/action-field.component.ts
+++ src/app/components/action-field/action-field.component.ts
@@ -8,6 +8,7 @@ import {Component, Input, OnInit} from '@angular/core';
 export class ActionFieldComponent implements OnInit {
   @Input() type: string;
   @Input() content: string;
+  @Input() link: string;
 
   constructor() { }
 
