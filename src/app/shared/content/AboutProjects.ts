0fa960642beff6fb6b7bf5081d0e98565139a6b4
gitprzemek <przemyslaw.sz.home@gmail.com>
2020-01-08 21:59:20 +0100

    projects styles etc

--- src/app/shared/content/AboutProjects.ts
+++ src/app/shared/content/AboutProjects.ts
@@ -1,44 +1,49 @@
 export const AboutProjects = [
   {
-    title: { en: '111', pl: 'pl111'},
-    description: { en: '111', pl: 'pl111'},
-    links: {demo: 'xxx', repo: 'xxx'},
-    photo: 'sss',
-    tech: ['Javascript', 'CSS', 'JQuery', 'Angular', 'Git', 'Photoshop' ]
+    title: { en: 'Portfolio', pl: 'Portfolio'},
+    description: { en: 'My portfolio page inspired by Heroes of Might & Magic 3 game.', pl: 'Moja strona portfolio inspirowana grą Heroes of Might & Magic 3.'},
+    links: {demo: 'https://gitprzemek.github.io/wow-horde-clock/', repo: 'https://github.com/gitprzemek/wow-horde-clock'},
+    photo: '../../../assets/images/projects-images/angel.gif',
+    tech: ['Angular 2+', 'CSS3 | SASS', 'JS | TypeScript', 'Photoshop', 'BEM', 'Flex | RWD']
   },
   {
-    title: { en: '222', pl: 'pl111'},
-    description: { en: '111', pl: 'pl111'},
-    links: {demo: 'xxx', repo: 'xxx'},
-    photo: 'sss',
-    tech: ['Javascript', 'CSS', 'JQuery', 'Angular', 'Git', 'Photoshop' ]
+    title: { en: 'U duzego kowboja', pl: 'U duzego kowboja'},
+    description: { en: 'Commercial project commissioned by a local restaurant.',
+      pl: 'Komercyjny projekt wykonany na zlecenie miejscowej restauracji.'},
+    links: {demo: 'http://uduzegokowboja.pl/', repo: 'https://github.com/gitprzemek/u_duzego_kowboja'},
+    photo: '../../../assets/images/projects-images/cowboy.webp',
+    tech: ['HTML5', 'CSS3 | SASS', 'JavaScript | JQuery', 'Photoshop', 'BEM', 'Flex | RWD' ]
   },
   {
-    title: { en: '333', pl: 'pl111'},
-    description: { en: '111', pl: 'pl111'},
-    links: {demo: 'xxx', repo: 'xxx'},
-    photo: 'sss',
-    tech: ['Javascript', 'CSS', 'JQuery', 'Angular', 'Git', 'Photoshop' ]
+    title: { en: 'Old portfolio', pl: 'Stare portfolio'},
+    description: { en: 'The project, and at the same time my portfolio page. Created the longest and most often improved.',
+      pl: 'Projekt, a jednocześnie moja strona portfolio. Tworzony najdłużej i najczęściej aktualizowany.'},
+    links: {demo: 'https://gitprzemek.github.io/portfolio/', repo: 'https://github.com/gitprzemek/portfolio'},
+    photo: '../../../assets/images/projects-images/nyancat.gif',
+    tech: ['HTML5', 'CSS3', 'JavaScript | JQuery', 'Photoshop', 'Gulp', 'Flex | RWD' ]
   },
   {
-    title: { en: '444', pl: 'pl111'},
-    description: { en: '111', pl: 'pl111'},
-    links: {demo: 'xxx', repo: 'xxx'},
-    photo: 'sss',
-    tech: ['Javascript', 'CSS', 'JQuery', 'Angular', 'Git', 'Photoshop' ]
+    title: { en: 'My movie library', pl: 'Moja biblioteka filmów'},
+    description: { en: 'An application containing movie lists based on the TMDB API, and on my own project made in Adobe XD.',
+      pl: 'Aplikacja zawierająca listy filmów bazująca na API serwisu TMDB, oraz na własnym projekcie wykonanym w Adobe XD.'},
+    links: {demo: 'https://gitprzemek.github.io/movie-library/index.html', repo: 'https://github.com/gitprzemek/movie-library'},
+    photo: '../../../assets/images/projects-images/bb82.gif',
+    tech: ['HTML5', 'CSS3 | SASS', 'JavaScript | ES6', 'PS | Adobe XD', 'Gulp | Babel', 'BEM' ]
   },
   {
-    title: { en: '555', pl: 'pl111'},
-    description: { en: '111', pl: 'pl111'},
-    links: {demo: 'xxx', repo: 'xxx'},
-    photo: 'sss',
-    tech: ['Javascript', 'CSS', 'JQuery', 'Angular', 'Git', 'Photoshop' ]
+    title: { en: 'Avengers vs Thanos', pl: 'Avengers vs Thanos'},
+    description: { en: 'Memory card game, inspired by the theme of the movie Avengers: Infinity War.',
+      pl: 'Gra typu Memory card, zainspirowana motywem z filmu Avengers: Infinity War.'},
+    links: {demo: 'https://gitprzemek.github.io/avengers-memory-card/', repo: 'https://github.com/gitprzemek/avengers-memory-card'},
+    photo: '../../../assets/images/projects-images/thanos.gif',
+    tech: ['HTML5', 'CSS3', 'JavaScript | ES6', 'Photoshop', 'Flex | RWD', ' ' ]
   },
   {
-    title: { en: '666', pl: 'pl111'},
-    description: { en: '111', pl: 'pl111'},
-    links: {demo: 'xxx', repo: 'xxx'},
-    photo: 'sss',
-    tech: ['Javascript', 'CSS', 'JQuery', 'Angular', 'Git', 'Photoshop' ]
+    title: { en: 'Warcraft clock', pl: 'Warcraft clock'},
+    description: { en: 'Disc clock in the style of World of Warcraft game.', pl: 'Zegar tarczowy w stylu gry World of Warcraft.'},
+    links: {demo: 'https://gitprzemek.github.io/wow-horde-clock/', repo: 'https://github.com/gitprzemek/wow-horde-clock'},
+    photo: '../../../assets/images/projects-images/tauren.gif',
+    tech: ['HTML5', 'CSS3', 'JavaScript | ES6', 'Photoshop', 'Flex | RWD', ' ' ]
   },
+
 ];
