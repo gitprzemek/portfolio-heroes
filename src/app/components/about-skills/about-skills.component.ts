f3282598815e2ef04cef390817de7d7ae770e92e
gitprzemek <przemyslaw.sz.home@gmail.com>
2020-01-14 19:39:32 +0100

    about skills finish

--- src/app/components/about-skills/about-skills.component.ts
+++ src/app/components/about-skills/about-skills.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
+import {MainServiceService} from "../../shared/services/main-service.service";
 
 @Component({
   selector: 'app-about-skills',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-skills.component.scss']
 })
 export class AboutSkillsComponent implements OnInit {
+  heightPercent: number = 0;
+  widthPercent: number = 0;
+  textFromLogo: string = '';
 
-  constructor() { }
-
-  ngOnInit() {
+  @HostListener('document:mousemove', ['$event'])
+  onMouseMove(e) {
+    const height = (e.clientY / e.view.innerHeight) * 100;
+    this.heightPercent = 49 + ((height / 100) * 4);
+    const width = (e.clientX / e.view.innerWidth) * 100;
+    this.widthPercent = 46 + ((width / 100) * 10);
   }
+  constructor(private _mainServiceService: MainServiceService) { }
 
+  ngOnInit(): void {
+  }
+  closeWindow(): void {
+    this._mainServiceService.showAboutSkills.next(false);
+  }
+  passTextToInput(text): void {
+    this.textFromLogo = text;
+  }
 }
