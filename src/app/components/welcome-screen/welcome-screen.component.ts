01854752ddef7a4dccb42d1c64192df0d11e61c2
gitprzemek <przemyslaw.sz.home@gmail.com>
2020-05-17 16:52:57 +0200

    fix wall z index and scroll

--- src/app/components/welcome-screen/welcome-screen.component.ts
+++ src/app/components/welcome-screen/welcome-screen.component.ts
@@ -1,36 +1,12 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {MainServiceService} from "../../shared/services/main-service.service";
-import {gsap} from "gsap"
-import { TimelineMax, CSSPlugin, ScrollToPlugin, Draggable } from "gsap/all";
-import {animate, keyframes, state, style, transition, trigger} from "@angular/animations";
+import {slideLeftAnimation} from "../../shared/animations/slideLeftWall";
+import {slideRightAnimation} from "../../shared/animations/slideRightWall";
 @Component({
   selector: 'app-welcome-screen',
   templateUrl: './welcome-screen.component.html',
   styleUrls: ['./welcome-screen.component.scss'],
-  animations: [
-    trigger('slideWall', [
-      transition('* => *', [
-        style({ transform: "translateX(0)", zIndex: "0" , offset: 0 }),
-        animate("1.6s", keyframes([
-          style({ transform: "translateX(-102%)", zIndex: "9999" , offset: 0 }),
-          style({ transform: "translateX(0)", zIndex: "9999" , offset: 0.42 }),
-          style({ transform: "translateX(0)", zIndex: "9999" , offset: 0.58 }),
-          style({ transform: "translateX(-102%)", zIndex: "9999" , offset: 1 }),
-        ]))
-      ])
-    ]),
-    trigger('slideWall2', [
-      transition('* => *', [
-        style({ transform: "translateX(0)", zIndex: "0" , offset: 0 }),
-        animate("1.6s", keyframes([
-          style({ transform: "translateX(102%)", zIndex: "9999" , offset: 0 }),
-          style({ transform: "translateX(0)", zIndex: "9999" , offset: 0.42 }),
-          style({ transform: "translateX(0)", zIndex: "9999" , offset: 0.58 }),
-          style({ transform: "translateX(102%)", zIndex: "9999" , offset: 1 }),
-        ]))
-      ])
-    ]),
-  ],
+  animations: [slideLeftAnimation, slideRightAnimation]
 })
 export class WelcomeScreenComponent implements OnInit {
   enterToApp = false;
@@ -54,22 +30,6 @@ export class WelcomeScreenComponent implements OnInit {
   }
   enterToAppFn(data: boolean): void {
     this._mainServiceService.enterToApp.next(data);
-    // gsap.fromTo("#svgImage", {scale:1}, {scale:8, duration: 3});
-    // gsap.fromTo("#svgImage", {scale: 1}, {duration: 2, scale: 9, ease: "expoScale(1, 9, power2.inOut)"});
-
-    // let tl = new TimelineMax({repeat: -1, yoyo: true, repeatDelay: 0.3}),
-    //   circle = document.getElementById("firstPath");
-  //
-  //   // tl.to(circle, {morphSVG:"#secondPath"}, "+=1")
-  //   //   .to(circle, {morphSVG:"#thirdPath"}, "+=1")
-  //   //   .to(circle, {morphSVG:circle}, "+=1")
-  // tl.to(circle, 1.3, { attr: { morphSVG:"#secondPath"}, ease: Back.easeInOut})
-  // .to(circle, 1.3, { attr: { morphSVG:"#thirdPath"}, ease: Back.easeInOut})
-
-    // let tlMorph = new TimelineMax({repeat: -1, yoyo: true, repeatDelay: 0.3})
-    //
-    // tlMorph
-    //   .to('#firstPath', 1, {morphSVG: {shape: '#secondPath'}})
   }
 
 }
---
+++ src/app/shared/animations/slideRightWall.ts
@@ -0,0 +1,14 @@
+import {animate, keyframes, style, transition, trigger} from "@angular/animations";
+
+export const slideRightAnimation =
+  trigger('slideWall2', [
+  transition('* => *', [
+    style({ transform: "translate3d(0, 0, 0, 0)", zIndex: "0" , offset: 0 }),
+    animate("1500ms cubic-bezier(0.285, 0.860, 0.790, 0.130)", keyframes([
+      style({ transform: "translate3d(102%, 0, 0)", zIndex: "9999" , offset: 0 }),
+      style({ transform: "translate3d(0, 0, 0)", zIndex: "9999" , offset: 0.44 }),
+      style({ transform: "translate3d(0, 0, 0)", zIndex: "9999" , offset: 0.56 }),
+      style({ transform: "translate3d(102%, 0, 0)", zIndex: "9999" , offset: 1 }),
+    ]))
+  ])
+]);
